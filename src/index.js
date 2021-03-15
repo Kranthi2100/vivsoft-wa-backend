b8ac92c317312e492cf122df5480b14dec73a854
Kranthi2100 <15088730+Kranthi2100@users.noreply.github.com>
2021-03-16 03:23:56 +0530

    Fix parse error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var app = require("express")();
 var fetch = require("isomorphic-fetch");
-var cors = require('cors')
+var cors = require("cors");
 
 const API = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.key}&q=20171&days=1`;
 
@@ -13,29 +13,28 @@ function parseHourlyForcast(data) {
   }));
 }
 
-app.get('/', cors(), async (req, res) {
-    try {
-      const apiRes = await fetch(API);
+app.get("/", cors(), async (req, res) => {
+  try {
+    const apiRes = await fetch(API);
 
-      if (apiRes.status === 200) {
-        const weatherData = await apiRes.json();
+    if (apiRes.status === 200) {
+      const weatherData = await apiRes.json();
 
-        
-        const data = {
-          current: {
-            temp_f: weatherData.current.temp_f
-          },
-          forecast: parseHourlyForcast(weatherData)
-        };
+      const data = {
+        current: {
+          temp_f: weatherData.current.temp_f
+        },
+        forecast: parseHourlyForcast(weatherData)
+      };
 
-        return res.status(200).json(data);
-      } else {
-        throw new Error("status not handled");
-      }
-    } catch (error) {
-      console.error(error);
-      return res.status(500);
+      return res.status(200).json(data);
+    } else {
+      throw new Error("status not handled");
     }
-  })
+  } catch (error) {
+    console.error(error);
+    return res.status(500);
+  }
+});
 
 app.listen(8080); //the server object listens on port 8080
