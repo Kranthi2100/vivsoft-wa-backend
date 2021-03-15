438f609180a31d32689f6824cd570d8fe22d8a4d
Kranthi2100 <15088730+Kranthi2100@users.noreply.github.com>
2021-03-16 02:52:51 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+var http = require("http");
+var fetch = require("isomorphic-fetch");
+
+const API = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.key}&q=20171&days=1`;
+
+function parseHourlyForcast(data) {
+  const hourlyData = data.forecast.forecastday[0].hour;
+
+  return Object.values(hourlyData).map((value) => ({
+    time: value.time,
+    temp_f: value.temp_f
+  }));
+}
+
+http
+  .createServer(async function (req, res) {
+    try {
+      const apiRes = await fetch(API);
+
+      if (apiRes.status === 200) {
+        const weatherData = await apiRes.json();
+
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        const data = {
+          current: {
+            temp_f: weatherData.current.temp_f
+          },
+          forecast: parseHourlyForcast(weatherData)
+        };
+
+        res.end(JSON.stringify(data));
+      } else {
+        throw new Error("status not handled");
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500);
+    }
+  })
+  .listen(8080); //the server object listens on port 8080
